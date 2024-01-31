853c3b0a6916e18faaf7eabd5b92b5fb172a8b10
giacomotolari <tolarigiacomo@gmail.com>
2024-01-31 09:26:38 +0100

    add scripts and example get and post handlers using curl

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.use(express.json()); // to support JSON-encoded bodies
 // CREATE A SERVER AND API  ENDPOINTS
 
 // GET / (root)
+
+// get data from the endpoint using curl: curl http://localhost:9999
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -28,11 +31,16 @@ const players = [
 ];
 
 // GET /players
+
+// get data from the endpoint using curl: curl http://localhost:9999/players
+
 app.get("/players", (req, res) => {
   res.send(players);
 });
 
 // GET /players/:id
+
+// get data from the endpoint using curl: curl http://localhost:9999/players/1  (get the player with id 1)
 
 app.get("/players/:id", (req, res) => {
   const id = parseInt(req.params.id);
@@ -57,6 +65,15 @@ const logEndPoints = () => {
 };
 
 // POST /add-player/ (create a new player)
+
+// add a new player using curl:
+//
+
+// curl -X POST http://localhost:9999/add-player -H "Content-Type: application/json" -d '{"name":"Michael Jordan","team":"Bulls","number":23}'
+
+// -X POST: to specify the method
+// -H (headers) "Content-Type: application/json": to specify the content type
+// -d (data) '{"name":"Michael Jordan","team":"Bulls","number":23}': to specify the data to send
 
 app.post("/add-player", (req, res) => {
   console.log("req.body:", req.body);
